512e5a179fb8fad5ac94f79d6eb65fb9d4348daa
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-27 13:45:42 +0500

    Changes

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+const index = () => {
+    return (
+        <div className='app' >
+            <h1 className='main-heading' >Bookmark App</h1>
+            <p className='main-para' >Track a list of your favorite websites</p>
+        </div>
+    )
+}
+
+export default index
